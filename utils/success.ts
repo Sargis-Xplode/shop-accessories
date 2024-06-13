228a2598affae1ab4404977a922e77f054663393
Sargis <165631492+Sargis-Xplode@users.noreply.github.com>
2024-06-13 09:42:27 +0400

    Update success.ts

--- utils/success.ts
+++ utils/success.ts
@@ -1,9 +1,11 @@
 import SuccessResponse from "types/success";
 
-export const Success = (success: boolean, message: string, data: any): SuccessResponse => {
+const Success = (success: boolean, message: string, data: any): SuccessResponse => {
     return {
         success,
         message,
         data,
     };
 };
+
+export default Success;
